09f0a84e265f85582fcd60e48cbd899aa54bb143
Evan Boehs <evan@boehs.org>
2023-03-01 15:47:47 -0500

    Simple and slightly incorrect but!!

--- gregre.ts
+++ gregre.ts
@@ -4,7 +4,7 @@ type PartialRecord<K extends keyof any, T> = Partial<Record<K, T>>
 
 type Methods = "get" | "post" | "put" | "post" | "patch" | "delete"
 
-type Endpoint = {
+type EndpointTyping = {
   parameters?: PartialRecord<"path" | "query", { [key: string]: string }>
   requestBody?: Record<string, any>
   responses: {
@@ -14,26 +14,42 @@ type Endpoint = {
   } | never
 }
 
+type Endpoint = Partial<{
+  [method in Methods]: EndpointTyping
+}>
+
 type Endpoints = {
-  [path: string]: Partial<{
-    [method in Methods]: Endpoint
-  }>
+  [path: string]: Endpoint
 }
 
 type InferResponse<
-  T extends Partial<{
-    [method in Methods]: Endpoint
-  }>,
+  T extends Endpoint,
   Method extends keyof T> = 'responses' extends keyof T[Method] ? T[Method]['responses'] : never
+  
+  type InferInput<
+  T extends Endpoint,
+  Method extends keyof T> = 'parameters' extends keyof T[Method] ? T[Method]['parameters'] : never
+
+type ToFns<T extends Endpoint> = {
+  [method in keyof T]: (params: InferInput<T,method>) => InferResponse<T,method> 
+}
 
 type PathToChain<
   E extends Endpoints,
-  Path extends keyof Endpoints,
+  Path extends keyof E,
   Original extends string = ''
   > = Path extends `/${infer P}`
   ? PathToChain<E, P, Path>
   : Path extends `${infer P}/${infer R}`
   ? { [K in P]: PathToChain<E, R, Original> }
   : {
-    [K in Path extends '' ? 'index' : Path]: E[Original]
+    [K in Path extends '' ? 'index' : Path]: ToFns<E[Original]>
   }
+
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+  k: infer I
+) => void
+  ? I
+  : never
+
+type ChainedPaths = UnionToIntersection<PathToChain<paths, keyof paths>>
